acabe69ed487d152c5fc174f762cf1b6d80497d7
Your Name <you@example.com>
2024-08-07 17:40:01 -0700

    update flake8

--- app/app.py
+++ app/app.py
@@ -1,14 +1,13 @@
 import openai
 import requests
-from flask import Flask, render_template, request, jsonify, Response, \
-    stream_with_context
+from flask import Flask, render_template, request, jsonify, Response, stream_with_context
 
 app = Flask(__name__)
 
 # Configure the OpenAI client to use the llamafile API
 openai.api_base = "http://localhost:8080/v1"  # llamafile server's address
 openai.api_key = "sk-no-key-required"
-LLAMAFILE_URL = "http://127.0.0.1:8080/completion"  # API endpoint
+LLAMAFILE_URL = "http://127.0.0.1:8080/"  # Update to match your API endpoint
 
 
 @app.route('/')
@@ -34,12 +33,8 @@ def get_llamafile_response(conversation):
         completion = openai.ChatCompletion.create(
             model="LLaMA_CPP",
             messages=[
-                {
-                "role": "system",
-                "content": "You are a travel assistant chatbot. Help users with "
-                "their travel-related queries. Provide useful website "
-                "links if possible to help users plan their trips."
-                },
+                {"role": "system",
+                 "content": "You are a travel assistant chatbot. Help users with their travel-related queries. Provide useful website links if possible to help users plan their trips."},
                 *conversation
             ]
         )
@@ -56,10 +51,13 @@ def chat_api_v1():
     # Get the conversation history from the request
     conversation = request.json.get('conversation', [])
 
+    #prompt = generate_prompt(conversation)
+
+
     # Prepare the payload for the llamafile API
     payload = {
-        "prompt": "You are a travel assistant chatbot. Help users with their "
-        "travel-related queries. You are in a conversation with a user"
+        "prompt": f"You are a travel assistant chatbot. Help users with their travel-related queries. "
+                  f"You are in a conversation with a user"
                   f"\n\n{generate_prompt(conversation)}\n\nBot:",
         "stream": True,  # Enable streaming
         "n_predict": 400,
@@ -68,18 +66,12 @@ def chat_api_v1():
     }
 
     # Send request to llamafile and stream response back to client
-    return Response(
-        stream_with_context(stream_llamafile_response(payload)),
-        content_type='text/event-stream'
-    )
+    return Response(stream_with_context(stream_llamafile_response(payload)), content_type='text/event-stream')
 
 
 def generate_prompt(conversation):
     # Generate a prompt from the conversation history
-    return "\n".join(
-        f"{msg['role'].capitalize()}: {msg['content']}"
-        for msg in conversation
-    )
+    return "\n".join(f"{msg['role'].capitalize()}: {msg['content']}" for msg in conversation)
 
 
 def stream_llamafile_response(payload):
